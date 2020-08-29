37fd10802a30b0f086f4a4883e6bccc868aba680
boomkim <bhk3177@gmail.com>
2020-08-29 19:32:47 +0900

    router 적용

--- src/App.js
+++ src/App.js
@@ -1,46 +1,66 @@
 import React, { Component } from 'react';
 
-import Register from './pages/Register'
+import Register from './pages/Register';
+import Home from './pages/Home';
 
-// import {
-//   BrowserRouter as Router,
-//   Switch,
-//   Route,
-//   Link
-// } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 
-
-class App extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      backendHealth: false
-    };
-  }
-
-  componentDidMount(){
-    fetch('/hello')
-    .then(res => {
-      console.log(res.status);
-      if (res.status === 200){
-        this.setState({
-          backendHealth: true
-        });
-      }
-    })
-  }
-  render() {
-    const isBackendHealthy = this.state.backendHealth
-    return (
-        <div>
-          <p>hello react</p>
-          <p>server health: {isBackendHealthy ? "Healthy!" : "Nope!" }</p>
-          <Register/>
-        </div>
-    );
-  }
+function App(props) {
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Plant Manager</Link>
+          </li>
+          <li>
+            <Link to="/register">회원가입</Link>
+          </li>
+        </ul>
+        <Route exact path="/" component={Home} />
+        <Route path="/register" component={Register} />
+      </div>
+    </Router>
+  );
 }
+
+// class App extends Component {
+
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       backendHealth: false
+//     };
+//   }
+
+//   componentDidMount(){
+//     fetch('/hello')
+//     .then(res => {
+//       console.log(res.status);
+//       if (res.status === 200){
+//         this.setState({
+//           backendHealth: true
+//         });
+//       }
+//     })
+//   }
+//   render() {
+//     const isBackendHealthy = this.state.backendHealth
+//     return (
+//         <div>
+//           <p>hello react</p>
+//           <p>server health: {isBackendHealthy ? "Healthy!" : "Nope!" }</p>
+//           <Register/>
+//         </div>
+//     );
+//   }
+// }
+
 
 export default App;
