b6428a11464f41c980333ee87a414452a4072382
boomkim <bhk3177@gmail.com>
2020-09-14 23:56:37 +0900

    navbar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import { Link, Route, Switch } from "react-router-dom";
 import MyPlants from "./MyPlants"
@@ -7,16 +7,26 @@ import MyPlants from "./MyPlants"
 function Home(props) {
   return (
     <div>
-      <Nav>
+      <Navbar className="navbar bg-light">
       <Container>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" 
+          data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" 
+          aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
         <Link className="navbar-brand" to="/">식물관리</Link>
         <Link to="/myplants">내 식물</Link>
+        <Link to="/me">내 정보</Link>
+        <Link to="/surf">둘러보기</Link>
+        <Link to="/logout">로그아웃</Link>
       </Container>
-      </Nav>
-      <Switch>
-        <Route path='/myplants'><MyPlants/></Route>
-        <Route path='/'><div>hello</div></Route>
-      </Switch>
+      </Navbar>
+      <Container>
+        <Switch>
+          <Route path='/myplants'><MyPlants/></Route>
+          <Route path='/'><div>hello</div></Route>
+        </Switch>
+      </Container>
     </div>
   )
 }
